2eb958a9b30bc6349d81072e83bbdc6db25bc803
AungPyaePhyoe22 <123373421+AungPyaePhyoe22@users.noreply.github.com>
2023-03-31 19:06:24 +0630

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "BANK.h"
+
+int main(){
+
+      loadingAllDataFromFile();
+      printingAllData();
+//      login();
+        space_counter();
+      registration();
+//    unsigned long long int PH = 0;
+//    printf("[X]Enter your Phone Number:");
+//    scanf("%llu",&PH);
+
+//    phone_validation(PH);
+
+//      char checkNrc[50];
+//      printf("Enter your NRC :");
+//      scanf(" %[^\n]",&checkNrc[0]);
+//
+//      nrc_validation(checkNrc);
+//        current_data_toTransfer(100);
+//        calculate_amounts_same_days(0);
+
+}
